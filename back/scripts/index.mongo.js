6ec0df497f31f14d3285b976d7614d5c789603d2
Imad MACHKOUR <imadmachkour1234@gmail.com>
2021-11-29 17:24:29 +0100

    scripts

--- back/scripts/index.mongo.js
+++ back/scripts/index.mongo.js
@@ -6,3 +6,13 @@ db.getCollection('links').createIndex(
     { name: 1},
     { unique: true }
 );
+
+db.getCollection('notes').createIndex(
+    { name: 1},
+    { unique: true }
+);
+
+db.getCollection('docs').createIndex(
+    { name: 1},
+    { unique: true }
+);
